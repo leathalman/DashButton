2444af790dc7686270d5e7a180eff9c149b1102c
Harrison Leath <hleath@me.com>
2019-04-06 15:41:45 -0400

    Update AppNotification.py

--- AppNotification.py
+++ AppNotification.py
@@ -3,10 +3,10 @@ import requests
 #Example for simple IFTTT webhook call
 
 def send_notification():
-	#will call IFTTT event for email, with user input for the 3 values
+	#will call IFTTT event for App Notification
 	#button_pressed is the "Event Name"
 	#"PLACEHOLDER" is the "Secret Key" from IFTTT
 	requests.post("https://maker.ifttt.com/trigger/button_pressed/with/key/PLACEHOLDER")    
 
-#will send a notification reading "Amazon Button pressed!" as configured by IFTTT website
+#will send a notification reading "Amazon Button pressed!" as configured by IFTTT
 #send_notification()
