23ff00fb07d2ac141b635252fb6e203ff6a9a46e
Harrison Leath <hleath@me.com>
2019-04-06 22:53:50 -0400

    Update AppleScripts.py

--- AppleScripts.py
+++ AppleScripts.py
@@ -2,17 +2,17 @@
 from subprocess import Popen, PIPE
 
 #any url can be replaced in the quotes below, I use google sheets as default
-WAKEUP_SCRIPT = '''
+SCRIPT = '''
 	tell application "Safari"
 		tell window 1
-			set current tab to (make new tab with properties {URL:"http://www.google.com"})
+			set current tab to (make new tab with properties {URL:"https://docs.google.com/spreadsheets/d/1fRhHG70r7nnoCD7CR0iObUyjg_oR_6ZAzPFh04Z-6Rs/edit#gid=0"})
 		end tell
 	end tell
 	'''
 
-def execute_applescript(packet_info, script=WAKEUP_SCRIPT):
+def execute_applescript(packet_info, script=SCRIPT):
 	p = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE)
 	p.communicate(script)
-	print("Action Executed")
+	print("AppleScript Executed")
 
 #execute_applescript(WAKEUP_SCRIPT)
