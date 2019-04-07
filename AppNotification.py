a65028d8236870a4be0f259c2d3d004dda9771e4
Harrison Leath <hleath@me.com>
2019-04-06 22:52:02 -0400

    Update AppNotification.py

--- AppNotification.py
+++ AppNotification.py
@@ -6,7 +6,8 @@ def send_notification():
 	#will call IFTTT event for App Notification
 	#button_pressed is the "Event Name"
 	#"PLACEHOLDER" is the "Secret Key" from IFTTT
-	requests.post("https://maker.ifttt.com/trigger/button_pressed/with/key/PLACEHOLDER")    
+	requests.post("https://maker.ifttt.com/trigger/button_pressed/with/key/PLACEHOLDER")
+	print("AppNotification Executed")
 
 #will send a notification reading "Amazon Button pressed!" as configured by IFTTT
 #send_notification()
