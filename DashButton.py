52e274f205221b98970c1058c5203fd49077f8f1
Harrison Leath <hleath@me.com>
2019-04-07 21:07:20 -0400

    Added tracking for total time spent

--- DashButton.py
+++ DashButton.py
@@ -2,7 +2,7 @@
 from scapy.all import *
 #Popen needed for applescipt integration in Python
 from subprocess import Popen, PIPE
-#requests used for webhooks with IFTTT
+#Requests used for webhooks with IFTTT
 import requests
 import datetime
 import GoogleSheets
@@ -10,8 +10,8 @@ import AppNotification
 import AppleScripts
 
 #Static var for the MAC Address of Dash Button beacuse I'm lazy
-DEVICE_MAC_ADDRESS = 'PUT MAC ADDRESS OF DASH BUTTON HERE'
-thirdColumn = "Default Message"
+DEVICE_MAC_ADDRESS = 'PUT MAC ADDRESS OF DASH BUTTON HERE' 
+THIRD_COLUMN = "Default Message"
 SCRIPT = AppleScripts.SCRIPT
 	
 def device_found(packet_info):
@@ -21,32 +21,36 @@ def execute_action(packet_info, script=SCRIPT):
 	if device_found(packet_info):
 		AppNotification.send_notification()
 		AppleScripts.execute_applescript(SCRIPT)
-		print("Action Executed at")
 
 x = 0
-#while x is less than 2, complete the if loop
 while x < 2:
-	#if x is even execute the following 
 	if x % 2 == 0:
 		sniff(prn=execute_action, filter="arp", stop_filter=device_found)
-		TIME = datetime.datetime.now()
-		FORMATTED_TIME = TIME.strftime("%I:%M %p, %m/%d/%y")
-		FIRST_COLUMN = TIME.strftime("%B %d, %Y")
-		SECOND_COLUMN = TIME.strftime("%I:%M %p")
-		thirdColumn = "Started Activity"
-		GoogleSheets.add_row(FIRST_COLUMN, SECOND_COLUMN, thirdColumn)
-		print(" ")
+		CURRENT_TIME = datetime.datetime.now()
+		#used to get the first time the button is pressed
+		OLD_TIME = datetime.datetime.now()
+		FORMATTED_TIME = CURRENT_TIME.strftime("%I:%M %p, %m/%d/%y")
+		FIRST_COLUMN = CURRENT_TIME.strftime("%B %d, %Y")
+		SECOND_COLUMN = CURRENT_TIME.strftime("%I:%M %p")
+		THIRD_COLUMN = "Started Activity"
+		GoogleSheets.add_row(FIRST_COLUMN, SECOND_COLUMN, THIRD_COLUMN)
 		print("Started Activity")
+		print(" ")
 		x += 1	
-	#if x is not even execute the following
-	else:
+	elif x % 2 == 1:
 		sniff(prn=execute_action, filter="arp", stop_filter=device_found)
 		TIME = datetime.datetime.now()
-		FORMATTED_TIME = TIME.strftime("%I:%M %p, %m/%d/%y")
-		FIRST_COLUMN = TIME.strftime("%B %d, %Y")
-		SECOND_COLUMN = TIME.strftime("%I:%M %p")
-		thirdColumn = "Ended Activity"
-		GoogleSheets.add_row(FIRST_COLUMN, SECOND_COLUMN, thirdColumn)
+		FORMATTED_TIME = CURRENT_TIME.strftime("%I:%M %p, %m/%d/%y")
+		FIRST_COLUMN = CURRENT_TIME.strftime("%B %d, %Y")
+		SECOND_COLUMN = CURRENT_TIME.strftime("%I:%M %p")
+		#subtract the OLD_TIME from the first button press from the CURRENT_TIME of the second button press
+		changeInTime = TIME - OLD_TIME
+		#used to get the days, minutes, and second between the timedelta of the first and second button presses
+		days = changeInTime.days
+		hours, remainder = divmod(changeInTime.seconds, 3600)
+		minutes, seconds = divmod(remainder, 60)
+		THIRD_COLUMN = "Ended Activity, %r hours %r minutes" % (hours, minutes)
+		GoogleSheets.add_row(FIRST_COLUMN, SECOND_COLUMN, THIRD_COLUMN)
+		print("Ended Activity, %r hours %r minutes" % (hours, minutes))
 		print(" ")
-		print("Ended Activity")
 		x += 1
